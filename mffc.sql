830f4a6db22ddb126aaee735caf115a6eb8ffef6
flyer <253311459@qq.com>
2014-10-26 10:14:52 +0800

    忘记commit的部分框架文件，同时更新了数据库

--- mffc.sql
+++ mffc.sql
@@ -6,3 +6,12 @@ primary key (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into article values ('','hello','hello world');
+
+create table user(
+id int not null auto_increment,
+mail varchar(225) not null,
+password varchar(225) not null,
+primary key (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into user values('','253311459@qq.com','flyer123');
