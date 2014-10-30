ab2463619214fd72eea199b98e6be626a89a2b69
flyer <253311459@qq.com>
2014-10-30 10:40:36 +0800

    删除大量无用ｃｓｓｊｓ

--- app/views/_layouts/default.blade.php
+++ app/views/_layouts/default.blade.php
@@ -4,26 +4,20 @@
 
 
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<title>flyers</title>
-<link rel="stylesheet" href="{{URL::asset('css/font-awesome.css')}}" type="text/css" >
-<link rel="stylesheet" id="contact-form-7-css" href="{{URL::asset('css/styles.css') }}" type="text/css" media="all">
-<link rel="stylesheet" id="hs_options_stylesheets_alt_style-css" href="{{URL::asset('css/style1.css') }}" type="text/css" media="all">
-<link rel="stylesheet" id="bootstrap-css" href="{{URL::asset('css/bootstrap.css')}}" type="text/css" media="all">
-<link rel="stylesheet" id="responsive-css" href="{{URL::asset('css/responsive.css')}}" type="text/css" media="all">
-<link rel="stylesheet" id="mainstyle-css" href="{{URL::asset('css/style.css') }}" type="text/css" media="all">
-<link rel="stylesheet" id="options_typography_Qwigley-css" href="{{URL::asset('css/css_003.css')}}" type="text/css" media="all">
-<link rel="stylesheet"  href="{{URL::asset('css/mycss.css')}}" type="text/css">
-<script type="text/javascript" src="{{URL::asset('js/jquery_002.js') }}"></script>
-<script type="text/javascript" src="{{URL::asset('js/jquery-migrate.js') }}"></script>
-<script type="text/javascript" src="{{URL::asset('js/modernizr.js') }}"></script>
-<script type="text/javascript" src="{{URL::asset('js/zilla-likes.js') }}"></script>
-<style type="text/css">.logo {
-    margin: 50px auto;
-}.top-panel-button .toggle-button {color:#e1e1e1}.top-panel-button {border-color:#eeeeee}.nav__primary {background:#ffffff}@media all and (min-width: 800px) {.flexnav > li > a:after {color:#d6d6d6}}.nav__primary {border-color:#eeeeee}.logo_tagline {color:#444444}.header-overlay {background:#ffffff}.header-overlay {-moz-opacity: 0.85; -khtml-opacity: 0.85; opacity: 0.85;}body { background-repeat:repeat; background-position:top center;  background-attachment:scroll; }body { background-color:#ffffff;}.headerstyler { background-color:#ffffff;}</style>
-<style type="text/css">
-.logo_h__txt, .logo_link { font-weight: 400; font-style: normal; font-size: 92px; line-height: 45px; font-family: Qwigley;  color:#222222; }
-</style>
+	<link rel="stylesheet" href="{{URL::asset('css/font-awesome.css')}}" type="text/css" >
+	<link rel="stylesheet" id="bootstrap-css" href="{{URL::asset('css/bootstrap.css')}}" type="text/css" media="all">
+	<link rel="stylesheet" id="mainstyle-css" href="{{URL::asset('css/style.css') }}" type="text/css" media="all">
+	<link rel="stylesheet" id="options_typography_Qwigley-css" href="{{URL::asset('css/css_003.css')}}" type="text/css" media="all">
+
+
+	<style type="text/css">.logo {
+		margin: 50px auto;
+	}.top-panel-button .toggle-button {color:#e1e1e1}.top-panel-button {border-color:#eeeeee}.nav__primary {background:#ffffff}@media all and (min-width: 800px) {.flexnav > li > a:after {color:#d6d6d6}}.nav__primary {border-color:#eeeeee}.logo_tagline {color:#444444}.header-overlay {background:#ffffff}.header-overlay {-moz-opacity: 0.85; -khtml-opacity: 0.85; opacity: 0.85;}body { background-repeat:repeat; background-position:top center;  background-attachment:scroll; }body { background-color:#ffffff;}.headerstyler { background-color:#ffffff;}</style>
+	<style type="text/css">
+		.logo_h__txt, .logo_link { font-weight: 400; font-style: normal; font-size: 92px; line-height: 45px; font-family: Qwigley;  color:#222222; }
+	</style>
 	
 </head>
 <body>
